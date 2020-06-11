fa2bbb3bff3b789267e4465e41f7b0fb71fec54b
Nick Southwell <nicksouthwell@yahoo.com>
2020-06-11 11:01:41 -0700

    Initial Skeleton

---
+++ src/main/java/com/nick/kidbank/KidBankApplication.java
@@ -0,0 +1,13 @@
+package com.nick.kidbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidBankApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KidBankApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nick/kidbank/KidBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.kidbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidBankApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
