729f2ce4ecd61308e60b01102543725185c61a1e
Nick Southwell <nicksouthwell@yahoo.com>
2020-06-11 11:23:00 -0700

    Fix tab size

--- src/main/java/com/nick/kidbank/KidBankApplication.java
+++ src/main/java/com/nick/kidbank/KidBankApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KidBankApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(KidBankApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(KidBankApplication.class, args);
+    }
 
 }
