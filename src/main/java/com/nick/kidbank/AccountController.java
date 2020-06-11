307ad4dacd654adb19119ad6390328a2332370cb
Nick Southwell <nicksouthwell@yahoo.com>
2020-06-11 11:53:30 -0700

    Initial web output of zero balance

---
+++ src/main/java/com/nick/kidbank/AccountController.java
@@ -0,0 +1,12 @@
+package com.nick.kidbank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/")
+    public String viewBalance() {
+        return "$0.00";
+    }
+}
