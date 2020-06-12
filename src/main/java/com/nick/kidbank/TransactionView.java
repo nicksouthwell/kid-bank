8cc24edbc2b3dbaa66ed8e099d7e61889bd7ca37
Nick Southwell <nicksouthwell@yahoo.com>
2020-06-12 11:14:40 -0700

    Show dummy deposit on web page

---
+++ src/main/java/com/nick/kidbank/TransactionView.java
@@ -0,0 +1,15 @@
+package com.nick.kidbank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionView {
+    private String date;
+    private String action;
+    private String amount;
+    private String source;
+}
